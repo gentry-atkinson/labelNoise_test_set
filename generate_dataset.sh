e7a4fab5b333423c13c901a6ecbea25025a87d79
gentry <gma23@txstate.edu>
2021-05-13 18:07:54 -0500

    made my script thing

---
+++ generate_dataset.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#Author: Gentry Atkinson
+#Organization: Texas University
+#Data: 13 May, 2021
+#Segment the raw data with clean labels, generate 6 noisy label sets, and then
+#  make a few visuals.
+
+echo Segmenting Data
+python3 divide_signals.py
+echo Generating NCAR
+pyton3 add_ncar.py
+echo Generating NAR
+python3 add_nar.py
+echo Generating NNAR
+python3 add_nnar.py
+echo Generating Plots
+python3 visualizer.py
