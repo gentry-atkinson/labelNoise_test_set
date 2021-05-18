a63ccc0351352d8ab146f4486eec5c877a266b67
gentry <gma23@txstate.edu>
2021-05-18 13:41:12 -0500

    get those utils

--- test_models_no_FE.py
+++ test_models_no_FE.py
@@ -6,10 +6,12 @@
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from os import listdir
 
-files = [
-        'clean_labels.csv'
-    ]
+files = [i for i in listdir('data') if 'label' in i]
 
 results_file = open('model_tests_no_fe.txt', 'w+')
 
@@ -21,8 +23,47 @@ if __name__ == '__main__':
     results_file.write('##### Model tests with No Feature Extraction #####\n\n')
 
     #test SVM
-    results_file.write('SVM with 80/20 split:\n')
+    results_file.write('SVM with rbf kernel and with 80/20 split:\n')
+    results_file.write('FILE\t\t\t\t\t\tACC\n')
     for file in files:
+        print('Reading ', file)
         y = np.genfromtxt('data/'+file)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        model = SVC().fit(X_train, y_train)
+        model = SVC(kernel='rbf').fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        results_file.write('{}{}{}\n'.format(file, '\t\t\t' if len(file) < 24  else '\t', accuracy_score(y_test, y_pred)))
+
+    #test Tree
+    results_file.write('\nTree with 80/20 split:\n')
+    results_file.write('FILE\t\t\t\t\t\tACC\n')
+    for file in files:
+        print('Reading ', file)
+        y = np.genfromtxt('data/'+file)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        model = tree.DecisionTreeClassifier().fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        results_file.write('{}{}{}\n'.format(file, '\t\t\t' if len(file) < 24  else '\t', accuracy_score(y_test, y_pred)))
+
+    #test KNN
+    results_file.write('\nKNN with K=3 and 80/20 split:\n')
+    results_file.write('FILE\t\t\t\t\t\tACC\n')
+    for file in files:
+        print('Reading ', file)
+        y = np.genfromtxt('data/'+file)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        model = KNeighborsClassifier(n_neighbors=3).fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        results_file.write('{}{}{}\n'.format(file, '\t\t\t' if len(file) < 24  else '\t', accuracy_score(y_test, y_pred)))
+
+    #test KNN again
+    results_file.write('\nKNN with K=7 and 80/20 split:\n')
+    results_file.write('FILE\t\t\t\t\t\tACC\n')
+    for file in files:
+        print('Reading ', file)
+        y = np.genfromtxt('data/'+file)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        model = KNeighborsClassifier(n_neighbors=7).fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        results_file.write('{}{}{}\n'.format(file, '\t\t\t' if len(file) < 24  else '\t', accuracy_score(y_test, y_pred)))
+
+    results_file.close()
